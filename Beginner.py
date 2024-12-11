5c5223dbcb933c1f9ef79a137a2ab460ef2b79e9
GomiSavvy <Brossister04@gmail.com>
2024-12-11 02:22:18 +0100

    learnt some new stuffs

--- Beginner.py
+++ Beginner.py
@@ -28,20 +28,66 @@
 # x = math.pi
 # print(x)
 
-import json
+# import json
 
-x = {
-    "name": "John",
-    "ade": 30,
-    "married": True,
-    "divorced": False,
-    "children": ("Ann", "Billy"),
-    "pets": None,
-    "cars": [
-        {"model": "Bmw 230", "mpg": 27.5},
-        {"model": "Ford Edge", "mpg": 24.1}
-    ]
-}
+# x = {
+#     "name": "John",
+#     "ade": 30,
+#     "married": True,
+#     "divorced": False,
+#     "children": ("Ann", "Billy"),
+#     "pets": None,
+#     "cars": [
+#         {"model": "Bmw 230", "mpg": 27.5},
+#         {"model": "Ford Edge", "mpg": 24.1}
+#     ]
+# }
 
-print(json.dumps(x))
-print(x['cars'][1])
+# print(json.dumps(x))
+# print(x['cars'][1])
+
+# import camelcase
+# import re
+# check if the string starts with "The" and ends with "Spain":
+# txt = "The rain in Spain"
+# x = re.search("^The.*Spain$", txt)
+# if x:
+#     print("Yes we have a match!")
+# else:
+#     print("No match")
+# x = re.findall("Portugal", txt)
+# print(x)
+# x = re.search("\s", txt)
+# print("The first whitespace character is located in position:", x.start())
+
+# splits the string at each whitespace and return a list
+# x = re.search(r"\bS\w+", txt)
+# print(x.group())
+
+
+# try:
+#     number = int(input("Enter a number: "))
+#     print(1 / number)
+
+# except ZeroDivisionError:
+#     print("You can't Divide by Zero IDIOT")
+# except ValueError:
+#     print("Enter only Numbers please!")
+# except Exception:
+#     print("Something went wrong")
+# finally:
+# #     print("Do some cleanup here")
+
+# f = open('file.txt')
+# try:
+#     f
+#     var = bad_var
+# except FileNotFoundError as e:
+#     print(e)
+# except Exception as e:
+#     print(e)
+# else:
+#     print(f.read())
+#     f.close
+# finally:
+#     print("Executing Finally...")
